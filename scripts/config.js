cb64c36b54327e430d58245219f27dd828483a1f
Пункт Б: Разработка <145120621+punktbDev@users.noreply.github.com>
2025-07-16 00:54:05 +0300

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,2 @@
-const API_URL = "https://vds.punkt-b.pro/api/v1"
-const CABINET_URL = "https://lk.punkt-b.pro/"
+const API_URL = "https://punktbdev-punktb-ea39.twc1.net/api/v1"
+const CABINET_URL = "https://punktbdev-cabinet-d4a4.twc1.net/"
